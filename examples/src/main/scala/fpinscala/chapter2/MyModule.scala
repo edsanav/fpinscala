47aebff47319db4e741ea20073a86648d4101fc8
Eduardo Santamaria <edsanav@gmail.com>
2019-07-18 22:28:52 +0200

    Chapter 2

--- examples/src/main/scala/fpinscala/chapter2/MyModule.scala
+++ examples/src/main/scala/fpinscala/chapter2/MyModule.scala
@@ -13,6 +13,16 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
+
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 }
