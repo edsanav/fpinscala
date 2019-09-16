ef9df32f8e47489916c382041726cab82ec36dc1
Eduardo Santamaria <edsanav@gmail.com>
2019-09-16 23:00:43 +0200

    Streams (take)

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -17,7 +17,12 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = ???
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if  n > 1 => cons(h(), t().take(n-1))
+    case Cons(h, _) if n == 1 => cons(h(), Empty)
+    case _ => Empty
+  }
 
   def drop(n: Int): Stream[A] = ???
 
