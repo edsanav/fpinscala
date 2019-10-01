ee77b4bc35f8e0e59f234065bbe95b4a3a0bae81
Eduardo Santamaria <edsanav@gmail.com>
2019-10-01 22:55:30 +0200

    TakeWhile with fold

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,6 +3,20 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  def toList:List[A] = this match {
+    case Cons(h,t) => h()::t().toList
+    case _ => List()
+  }
+
+  def toListHelper:List[A] = {
+    @annotation.tailrec
+    def go(next:Stream[A], accum:List[A]):List[A] = next match {
+      case Cons(h,t) => go(t(), h()::accum)
+      case _ => accum
+    }
+    go(this, List()).reverse
+  }
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -19,21 +33,26 @@ trait Stream[+A] {
   }
 
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if  n > 1 => cons(h(), t().take(n-1))
-    case Cons(h, _) if n == 1 => cons(h(), Empty)
+    case Cons(h, t) if  n >= 1 => cons(h(), t().take(n-1))
     case _ => Empty
   }
 
   def drop(n: Int): Stream[A] = this match {
     case Cons(_, t) if n > 1 => t().drop(n-1)
     case Cons(_, t) if n == 1 => t()
-    case xs => this
+    case _ => this
   }
 
   def takeWhile(p: A => Boolean): Stream[A] = this match {
     case Cons(h,t) if p(h()) => cons(h(), t().takeWhile(p))
     case _ => empty
   }
+
+  def takeWhileFold(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h,t)=>{
+      if (p(h)) cons(h,t.takeWhile(p))
+      else empty
+    })
 
   def forAll(p: A => Boolean): Boolean = this match {
     case Cons(h, t) if p(h()) => t().forAll(p)
