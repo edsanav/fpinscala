0c37c0a165c929f1cdcf4a4eff352ae4422182f7
Eduardo Santamaria <edsanav@gmail.com>
2019-11-23 19:59:34 +0100

    In terms of flatmap

--- answerkey/parsing/07.answer.scala
+++ answerkey/parsing/07.answer.scala
@@ -1,5 +1,6 @@
 /* 
-These can be implemented using a for-comprehension, which delegates to the `flatMap` and `map` implementations we've provided on `ParserOps`, or they can be implemented in terms of these functions directly. 
+These can be implemented using a for-comprehension, which delegates to the `flatMap` and `map` implementations
+we've provided on `ParserOps`, or they can be implemented in terms of these functions directly.
 */
 def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)] = 
   flatMap(p)(a => map(p2)(b => (a,b)))
--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,95 +1,124 @@
 package fpinscala.parsing
 
 import fpinscala.testing._
+
 import language.higherKinds
+import scala.util.matching.Regex
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait Parsers[Parser[+ _]] {
+  self => // so inner classes may call methods of trait
 
-  def run[A](p:Parser[A])(input: String):Either[ParseError, A]
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def regex(r: Regex): Parser[String]
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+  def slice[A](p: Parser[A]): Parser[String]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = p.flatMap(a => succeed(f(a)))
 
   def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
 
-  def listOfN[A](n:Int, p:Parser[A]):Parser[List[A]]
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = p.flatMap(
+    (a: A) => p2.map((b: B) => f(a, b))
+  )
 
-  def many[A](p: Parser[A]): Parser[List[A]]
+  def map2WithProduct[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] = product(p, p2).map(f.tupled)
 
-  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] = p.flatMap((a:A) => p2.map((b: B) => (a,b)))
 
-  def flatMap[A,B](a: Parser[A])(f: A => Parser[B]): Parser[B]
+  def many[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _) or succeed(List[A]())
 
-  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] = {
-      flatMap(p)((a:A) => map(p2)((b:B) => f(a,b)))
-    }
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = {
+    if (n <= 0) succeed(List[A]())
+    else map2(p, listOfN(n - 1, p))(_ :: _)
+  }
 
-  def map2WithProduct[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] = map(product(p, p2))(f.tupled)
+  def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _)
 
-  def product[A,B](a:Parser[A],b:Parser[B]):Parser[(A,B)] = map2(a,b)((_,_))
 
-  def many[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_::_)
-
-  def succeed[A](a: A):Parser[A] = string("").map(_ => a) // succedded parser (checks with empty string and returns imput)
+  def succeed[A](a: A): Parser[A] = string("").map(_ => a) // succedded parser (checks with empty string and returns imput)
 
 
   //def count[A](p:Parser[List[A]]):Parser[Int] // Too speciffic
-  def count[A](p:Parser[Char]):Parser[Int] = map(many("a"))(_.size)
+  def count[A](p: Parser[Char]): Parser[Int] = many(p).map(_.size)
 
-  def slice[A](p: Parser[A]): Parser[String]
+  def numAs[A]:Parser[List[String]] = "^[0-9]*".r.slice.flatMap(s => listOfN(s.toInt, "a"))
 
+  def unbiasL[A, B, C](p: ((A, B), C)): (A, B, C) = (p._1._1, p._1._2, p._2)
 
-  def or[A](s1:Parser[A], s2:Parser[A]): Parser[A]
-  implicit def string(s:String): Parser[String]
-  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
-
-  def and[A](s1:Parser[A], s2:Parser[A]): Parser[A]
-
+  def unbiasR[A, B, C](p: (A, (B, C))): (A, B, C) = (p._1, p._2._1, p._2._2)
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
-    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
-    def &[B>:A](p2: Parser[B]): Parser[B] = self.and(p, p2)
-    def and[B>:A](p2: Parser[B]): Parser[B] = self.and(p, p2)
+    def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
-    def **[B>:A](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
-    def product[B>:A](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
+    def **[B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
+    def product[B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
-    def many:Parser[List[A]] = self.many(p)
-    def flatMap[B](f:A=>Parser[B]):Parser[B] = self.flatMap(p)(f)
-    def map[B](f:A=>B):Parser[B] = self.map(p)(f)
+    def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
 
-    def slice:Parser[String] = self.slice(p)
+    def many: Parser[List[A]] = self.many(p)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def slice: Parser[String] = self.slice(p)
 
   }
 
   object Laws {
 
-    def equal[A](p1:Parser[A], p2:Parser[A])(in:Gen[String]):Prop =
-      Prop.forAll(in)(s=>run(p1)(s) == run(p2)(s))
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
 
-    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =  equal(p, map(p)(a => a))(in)
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop = equal(p, map(p)(a => a))(in)
 
-    def succeedLaw[A](in:Gen[String])(in2:Gen[A]): Prop =
-      Prop.forAll(in **in2 ){case (s, a) => run(succeed(a))(s) == Right(a)}
+    def succeedLaw[A](in: Gen[String])(in2: Gen[A]): Prop =
+      Prop.forAll(in ** in2) { case (s, a) => run(succeed(a))(s) == Right(a) }
+
+    def productLaw1[A](p1: Parser[A], p2: Parser[A], p3: Parser[A])(in: Gen[String]): Prop =
+      equal(
+        ((p1 ** p2) ** p3) map (unbiasL),
+        (p1 ** (p2 ** p3)) map (unbiasR)
+      )(in)
+
+    def productLaw2[A, B](p1: Parser[A], p2: Parser[A])(f: A => B, g: A => B)(in: Gen[String]): Prop =
+      equal(
+        p1.map(f) ** p2.map(g),
+        p1 ** p2 map { case (a, b) => (f(a), g(b)) }
+      )(in)
+
   }
+
 }
 
 case class Location(input: String, offset: Int = 0) {
 
-  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val line = input.slice(0, offset + 1).count(_ == '\n') + 1
+  lazy val col = input.slice(0, offset + 1).reverse.indexOf('\n')
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
 
-  def advanceBy(n: Int) = copy(offset = offset+n)
+  def advanceBy(n: Int) = copy(offset = offset + n)
 
   /* Returns the line corresponding to this location */
-  def currentLine: String = 
-    if (input.length > 1) input.lines.drop(line-1).next
+  def currentLine: String =
+    if (input.length > 1) input.lines.drop(line - 1).next
     else ""
 }
 
-case class ParseError(stack: List[(Location,String)] = List(),
+case class ParseError(stack: List[(Location, String)] = List(),
                       otherFailures: List[ParseError] = List()) {
 }
