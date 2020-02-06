77220d2b1a9bd17bf61cf6db7638ee0c1b566ac5
Eduardo Santamaria <edsanav@gmail.com>
2020-02-06 22:54:23 +0100

    State monad as taken from example

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -89,7 +89,30 @@ object Monad {
     override def flatMap[A,B](ma: List[A])(f: A => List[B]): List[B] = ma.flatMap(f)
   }
 
-  def stateMonad[S] = ???
+  // Since `State` is a binary type constructor, we need to partially apply it
+  // with the `S` type argument. Thus, it is not just one monad, but an entire
+  // family of monads, one for each type `S`. One solution is to create a class
+  // `StateMonads` that accepts the `S` type argument and then has a _type member_
+  // for the fully applied `State[S, A]` type inside:
+  class StateMonads[S] {
+    type StateS[A] = State[S,A]
+
+    // We can then declare the monad for the `StateS` type constructor:
+    val monad = new Monad[StateS] {
+      def unit[A](a: => A): State[S, A] = State(s => (a, s))
+      override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+        st flatMap f
+    }
+
+  }
+
+  //Alternative inlined version
+  // https://underscore.io/blog/posts/2016/12/05/type-lambdas.html
+  def stateMonad[S] = new Monad[({type f[x] = State[S, x]})#f] {
+    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
 
   val idMonad: Monad[Id] = ???
 
